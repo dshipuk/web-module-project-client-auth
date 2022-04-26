ae8041b7ed0248d9f9a968b03e7121de768e125c
unknown <dshipuk@yahoo.com>
2022-04-26 14:16:43 -0700

    Added Links to login, friendslist, addfriends, logout and made login work

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Redirect, Link } from "react-router-dom";
 
-const Login = () => {
-  return (
-    <h2>Login</h2>
-  )
-}
-
-const FriendsList = () => {
-  return (
-    <h2>FriendsList</h2>
-  )
-}
-
-const AddFriend = () => {
-  return (
-    <h2>AddFriend</h2>
-  )
-}
-
+import Login from "./components/Login";
+import FriendsList from "./components/FriendsList";
+import AddFriend from "./components/AddFriend";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Route exact path="/">
-        <Login />
-      </Route>
-      <Route exact path="/login">
-        <Redirect to="/" />
-      </Route>
-      <Route exact path="/friends">
-        <FriendsList />
-      </Route>
-      <Route exact path="/friends/add">
-        <AddFriend />
-      </Route>
-    </div>
+      <div className="App">
+        <header>
+          <h2>Friends Database</h2>
+          <Link className="link" to="login">Login</Link>
+          <Link className="link" to="friends">FriendsList</Link>
+          <Link className="link" to="friends/add">Add Friends</Link>
+          <Link className="link" to ="friends">Logout</Link>
+        </header>
+        <Route exact path="/">
+          <Login />
+        </Route>
+
+        <Route exact path="/login">
+          <Redirect to="/" />
+        </Route>
+
+        <Route exact path="/friends">
+          <FriendsList />
+        </Route>
+
+        <Route exact path="/friends/add">
+          <AddFriend />
+        </Route>
+      </div>
     </Router>
   );
 }
---
+++ src/components/FriendsList.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const FriendsList = () => {
+    const 
+
+  return (
+      <div>
+          <h2>FriendsList</h2>
+          <ul>
+              <li>Name - Age - Email</li>
+              <li>Name - Age - Email</li>
+              <li>Name - Age - Email</li>
+              <li>Name - Age - Email</li>
+              <li>Name - Age - Email</li>
+          </ul>
+      </div>
+  );
+};
+
+export default FriendsList;
